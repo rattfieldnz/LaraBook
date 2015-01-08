6012efb0652362e7e9fa92f68bc1beca86501cd1
Rob Attfield <attfire1@student.op.ac.nz>
2015-01-08 17:46:26 +1300

    Tidying up database config, creating master page and PagesController.

---
+++ app/controllers/PagesController.php
@@ -0,0 +1,8 @@
+<?php
+
+class PagesController extends \BaseController {
+	public function home()
+	{
+		return View::make('pages.home');
+	}
+}
---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="content">
+			@yield('content')
+		</div>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	</body>
+</html>
